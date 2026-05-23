eee85fb36cf7c8db05c037db9b844069858a8c86
jdw5 <joshuadwallace1@gmail.com>
2026-05-23 00:08:38 +0000

    feat(abn): bump deps, coverage

--- src/Style/Rgb.php
+++ src/Style/Rgb.php
@@ -40,7 +40,7 @@ class Rgb implements Stringable
     /**
      * Get the string representation of the RGB color.
      *
-     * @see Rgb::__toString()
+     * @see \Honed\Chart\Style\Rgb::__toString()
      */
     public function toString(): string
     {
@@ -50,7 +50,7 @@ class Rgb implements Stringable
     /**
      * Get the string representation of the RGB color.
      *
-     * @see Rgb::__toString()
+     * @see \Honed\Chart\Style\Rgb::__toString()
      */
     public function value(): string
     {
